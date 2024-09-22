4bff9e5ea61e1a8699759e90c3dbea195df27888
Dmitrii Lobanov <dvmald@icloud.com>
2024-09-22 23:49:03 +0300

    update

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Devices",
+    platforms: [
+        .iOS(.v16) // Указываем минимальную версию iOS
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
